6dcd50b6c5bd9bf161c40ee0407b0f6197a88cdc
faisal alfariheedi <falfariheedi@gmail.com>
2021-12-01 11:40:32 +0300

    Initial Commit

---
+++ AddingtotheBeastListTests/AddingtotheBeastListTests.swift
@@ -0,0 +1,33 @@
+//
+//  AddingtotheBeastListTests.swift
+//  AddingtotheBeastListTests
+//
+//  Created by faisal on 26/04/1443 AH.
+//
+
+import XCTest
+@testable import AddingtotheBeastList
+
+class AddingtotheBeastListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
